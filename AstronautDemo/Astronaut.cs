2b137152b014766281a3fa0c6876768991e7fcb2
300621334 <shafiqu@my.centennialcollege.ca>
2016-09-19 17:41:02 -0400

    initial commit

---
+++ AstronautDemo/Astronaut.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronautDemo
+{
+   public class Astronaut
+    {
+       //fields
+       private static int ASTRONAUT_COUNT = 0;
+       private static int THRESHOLD = 5;
+       //properties
+       public  string Name { get; private set; }
+       public  string Nationality { get; private set; }
+
+       private Astronaut(string name, string nationality)
+       {
+           Name = name;
+           Nationality = nationality;
+           ASTRONAUT_COUNT++;
+       }
+
+       public override string ToString()
+       {
+           return string.Format("Name: {0}  {1}  {2}", Name, Nationality, ASTRONAUT_COUNT);
+       }
+
+       public static Astronaut CreateAstronaut(string name, string nationality)
+       {
+           if(ASTRONAUT_COUNT < THRESHOLD)
+           {
+               return new Astronaut(name, nationality);
+               //if put count++ AFT return, gives unreachable code err.
+           }
+           else
+           {
+               return null;
+           }
+       }
+       //static method to access static+pvt COUNT field to change it
+       public static void changeThreshold(int newThreshold)
+       {
+           THRESHOLD = newThreshold;
+       }
+    }
+}
---
+++ AstronautDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AstronautDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Astronaut.changeThreshold(9);
+            List<Astronaut> astrList = new List<Astronaut>();
+            Astronaut ast1 =  Astronaut.CreateAstronaut("Shafiq", "Canadian");
+            //Console.WriteLine(ast1);
+            astrList.Add(ast1);
+            Astronaut ast2 = Astronaut.CreateAstronaut("John", "USA");
+            astrList.Add(ast2);
+            Astronaut ast3 = Astronaut.CreateAstronaut("Chris", "UK");
+            astrList.Add(ast3);
+            Astronaut ast4 = Astronaut.CreateAstronaut("Shafiq", "Canadian");
+            astrList.Add(ast4);
+            Astronaut ast5 = Astronaut.CreateAstronaut("John", "USA");
+            astrList.Add(ast5);
+            Astronaut ast6 = Astronaut.CreateAstronaut("Chris", "UK");
+            astrList.Add(ast6);
+            
+            foreach (Astronaut x in astrList)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
